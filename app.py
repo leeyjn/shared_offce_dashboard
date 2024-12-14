bd2238cb93366c78257939a4b00a258b17b53399
Eugene Lee <zrayonk@gmail.com>
2024-12-15 05:54:08 +0900

    updated serviceAccountKey.json path and QR code integration path

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, db
 import qrcode
 
 # Firebase 초기화
-cred = credentials.Certificate("serviceAccountKey.json")  # JSON 키 파일 경로
+cred = credentials.Certificate("C:\Users\pc\Python_Projects\shared_office_dashboard\serviceAccountKey.json")  # JSON 키 파일 경로
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://shareoffice-dashboard-default-rtdb.asia-southeast1.firebasedatabase.app/'
 })
@@ -23,7 +23,7 @@ def generate_qr(link, file_name="feedback_qr.png"):
     img.save(file_name)
 
 # QR 코드 생성
-generate_qr("https://sharedofficedashboard.streamlit.app/pages/survey", "feedback_qr.png")
+generate_qr("https://sharedofficedashboard-5rh9g5yyyfpszwi4cvbxax.streamlit.app/pages/survey", "feedback_qr.png")
 
 # Streamlit 시작
 st.set_page_config(page_title="공유 오피스 대시보드", layout="wide")
