e09453f51eff53479ed5f7ebf43cbf32cb122316
Eugene Lee <zrayonk@gmail.com>
2024-12-15 09:55:23 +0900

    update qrgenerator

--- generate_qr.py
+++ generate_qr.py
@@ -1,12 +1,18 @@
 import qrcode
 
-def generate_qr(link: str, file_name: str = "feedback_qr.png"):
-    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+def generate_qr(link, file_name="survey_qr.png"):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+    )
     qr.add_data(link)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(file_name)
+    print(f"QR 코드가 생성되었습니다: {file_name}")
 
-# Typeform 링크에 대한 QR 코드 생성
-typeform_link = "https://nwdkhtkycyq.typeform.com/to/kPCn5Xgy"
-generate_qr(typeform_link)
+# Typeform 설문 링크 (여기에 Typeform 설문 URL 넣기)
+typeform_url = "https://nwdkhtkycyq.typeform.com/to/kPCn5Xgy"
+generate_qr(typeform_url)
